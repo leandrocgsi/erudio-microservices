98d4c6111ebc7a338d8163511b98088dd042f150
leandrocgsi <leandrocgsi@gmail.com>
2021-04-08 17:08:00 -0300

    Initial commit

---
+++ 00-foo-bar-spring-initializr/src/main/java/br/com/erudio/BarApplication.java
@@ -0,0 +1,13 @@
+package br.com.erudio;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BarApplication.class, args);
+	}
+
+}
