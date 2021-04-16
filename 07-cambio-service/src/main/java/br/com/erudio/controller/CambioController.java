cd226f001191e19a2ac0d782d679d839dc00d019
leandrocgsi <leandrocgsi@gmail.com>
2021-04-16 13:55:22 -0300

    Circuit Breaker with Resilience4j

---
+++ 07-cambio-service/src/main/java/br/com/erudio/controller/CambioController.java
@@ -0,0 +1,43 @@
+package br.com.erudio.controller;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.erudio.model.Cambio;
+import br.com.erudio.repository.CambioRepository;
+
+@RestController
+@RequestMapping("cambio-service")
+public class CambioController {
+	
+	@Autowired
+	private Environment environment;
+	
+	@Autowired
+	private CambioRepository repository;
+	
+	@GetMapping(value = "/{amount}/{from}/{to}")
+	public Cambio getCambio(
+			@PathVariable("amount") BigDecimal amount,
+			@PathVariable("from") String from,
+			@PathVariable("to") String to
+			) {
+
+		var cambio = repository.findByFromAndTo(from, to);
+		if (cambio == null) throw new RuntimeException("Currency Unsupported");
+		
+		var port = environment.getProperty("local.server.port");
+		BigDecimal conversionFactor = cambio.getConversionFactor();
+		BigDecimal convertedValue = conversionFactor.multiply(amount);
+		cambio.setConvertedValue(convertedValue.setScale(2, RoundingMode.CEILING));
+		cambio.setEnvironment(port);
+		return cambio;
+	}
+}
