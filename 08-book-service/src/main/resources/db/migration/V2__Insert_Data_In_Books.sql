49669f275226bf6c5ee05af05f2c3f0c566eecc7
leandrocgsi <leandrocgsi@gmail.com>
2021-04-19 21:38:10 -0300

    Working with Spring Doc OpenAPI (Swagger)

---
+++ 08-book-service/src/main/resources/db/migration/V2__Insert_Data_In_Books.sql
@@ -0,0 +1,16 @@
+﻿INSERT INTO `book` (`author`, `launch_date`, `price`, `title`) VALUES
+	('Michael C. Feathers', '2017-11-29 13:50:05.878000', 8.57, 'Working effectively with legacy code'),
+	('Ralph Johnson, Erich Gamma, John Vlissides e Richard Helm', '2017-11-29 15:15:13.636000', 7.87, 'Design Patterns'),
+	('Robert C. Martin', '2009-01-10 00:00:00.000000', 13.46, 'Clean Code'),
+	('Crockford', '2017-11-07 15:09:01.674000', 11.71, 'JavaScript'),
+	('Steve McConnell', '2017-11-07 15:09:01.674000', 10.14, 'Code complete'),
+	('Martin Fowler e Kent Beck', '2017-11-07 15:09:01.674000', 15.38, 'Refactoring'),
+	('Eric Freeman, Elisabeth Freeman, Kathy Sierra, Bert Bates', '2017-11-07 15:09:01.674000', 19.23, 'Head First Design Patterns'),
+	('Eric Evans', '2017-11-07 15:09:01.674000', 16.09, 'Domain Driven Design'),
+	('Brian Goetz e Tim Peierls', '2017-11-07 15:09:01.674000', 13.99, 'Java Concurrency in Practice'),
+	('Susan Cain', '2017-11-07 15:09:01.674000', 21.51, 'O poder dos quietos'),
+	('Roger S. Pressman', '2017-11-07 15:09:01.674000', 9.79, 'Engenharia de Software: uma abordagem profissional'),
+	('Viktor Mayer-Schonberger e Kenneth Kukier', '2017-11-07 15:09:01.674000', 9.44, 'Big Data: como extrair volume, variedade, velocidade e valor da avalanche de informação cotidiana'),
+	('Richard Hunter e George Westerman', '2017-11-07 15:09:01.674000', 16.61, 'O verdadeiro valor de TI'),
+	('Marc J. Schiller', '2017-11-07 15:09:01.674000', 7.87, 'Os 11 segredos de líderes de TI altamente influentes'),
+	('Aguinaldo Aragon Fernandes e Vladimir Ferraz de Abreu', '2017-11-07 15:09:01.674000', 9.44, 'Implantando a governança de TI');
